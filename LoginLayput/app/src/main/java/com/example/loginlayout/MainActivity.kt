47752839e47dbe2f338249356e371be267d78d61
ALVF <arindhimar116@gmail.com>
2024-03-30 19:32:07 +0530

    login

---
+++ LoginLayput/app/src/main/java/com/example/loginlayout/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.loginlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+}
