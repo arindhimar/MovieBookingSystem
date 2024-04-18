c7b280c7be7d3cd0d564fa1f3974df8ecced0031
ALVF <arindhimar116@gmail.com>
2024-04-18 11:43:52 +0530

    added autmoatic slideview in image caroudel

--- CarouselImage/app/src/main/java/com/example/carouselimage/MainActivity.kt
+++ CarouselImage/app/src/main/java/com/example/carouselimage/MainActivity.kt
@@ -5,22 +5,30 @@ import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import android.widget.ImageView
 import java.util.ArrayList
+import java.util.Timer
+import java.util.TimerTask
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentIndex = 0
+    private val imageUrls = ArrayList<String>()
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ImageAdapter
+    private val handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView: RecyclerView = findViewById(R.id.recycler)
-        val arrayList: ArrayList<String> = ArrayList()
+        recyclerView = findViewById(R.id.recycler)
 
         // Add multiple images to the arrayList.
-        arrayList.add("https://imgkub.com/images/2024/04/01/image88c72a9d6bf66500.png")
-        arrayList.add("https://imgkub.com/images/2024/04/01/image597b74fe77157aae.png")
-        arrayList.add("https://imgkub.com/images/2024/04/01/image1cec867db9c206dc.png")
+        imageUrls.add("https://imgkub.com/images/2024/04/01/image88c72a9d6bf66500.png")
+        imageUrls.add("https://imgkub.com/images/2024/04/01/image597b74fe77157aae.png")
+        imageUrls.add("https://imgkub.com/images/2024/04/01/image1cec867db9c206dc.png")
 
-
-        val adapter = ImageAdapter(this@MainActivity, arrayList)
+        adapter = ImageAdapter(this@MainActivity, imageUrls)
         recyclerView.adapter = adapter
 
         adapter.setOnItemClickListener(object : ImageAdapter.OnItemClickListener {
@@ -28,5 +36,24 @@ class MainActivity : AppCompatActivity() {
                 // Do something like opening the image in a new activity or showing it in full screen or something else.
             }
         })
+
+        startAutoScroll()
     }
+
+    private fun startAutoScroll() {
+        val timer = Timer()
+        val task = object : TimerTask() {
+            override fun run() {
+                handler.postDelayed({
+                    currentIndex = (currentIndex + 1) % imageUrls.size
+                    recyclerView.smoothScrollToPosition(currentIndex)
+                }, 100) // Delay the initial smooth scroll operation by 100 milliseconds
+            }
+        }
+
+        // Schedule the task to run every 3 seconds
+        timer.schedule(task, 0, 3000)
+    }
+
+
 }
--- NavigationDrawer/app/src/main/java/com/example/navdrawer/MainActivity.kt
+++ NavigationDrawer/app/src/main/java/com/example/navdrawer/MainActivity.kt
@@ -2,18 +2,65 @@ package com.example.navdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import com.example.navdrawer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.ToolBaar)
+
         binding.ToolBaar.setOnClickListener {
-            binding.drawerLayout.open()
+            binding.drawerLayout.openDrawer(binding.navView)
         }
 
+        binding.navView.setNavigationItemSelectedListener { menuItem ->
+            // Set checked state for the selected item
+            menuItem.isChecked = true
+            // Close the drawer when item is tapped
+            binding.drawerLayout.closeDrawers()
+            // Handle navigation item clicks here
+            when (menuItem.itemId) {
+                R.id.nav_manage_screen -> {
+                    // Handle Manage Screen click
+                    Log.d("TAG", "Manage Screen clicked")
+                }
+                R.id.nav_manage_show -> {
+                    // Handle Manage Shows click
+                    Log.d("TAG", "Manage Shows clicked")
+                }
+                R.id.nav_view_booking -> {
+                    // Handle View Bookings click
+                    Log.d("TAG", "View Bookings clicked")
+                }
+                R.id.nav_menu_account -> {
+                    // Handle My Account click
+                    Log.d("TAG", "My Account clicked")
+                }
+                R.id.logoutuser -> {
+                    // Handle Logout click
+                    Log.d("TAG", "Logout clicked")
+                }
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            if (binding.drawerLayout.isDrawerOpen(binding.navView)) {
+                binding.drawerLayout.closeDrawer(binding.navView)
+            } else {
+                binding.drawerLayout.openDrawer(binding.navView)
+            }
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
