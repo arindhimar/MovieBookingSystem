cd9cf7a3d08e4f348b1e6db58601f36eab87bc14
ALVF <arindhimar116@gmail.com>
2024-05-21 15:52:41 +0530

    adding ui for user

---
+++ BottomNavigationTry/app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.example.bottomnavigationtry"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.bottomnavigationtry"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures{
+        viewBinding = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("com.github.qamarelsafadi:CurvedBottomNavigation:0.1.3")
+
+}
---
+++ Combined_Login+Register/app/src/main/java/com/example/combined_loginregister/UserActivity.kt
@@ -0,0 +1,19 @@
+package com.example.combined_loginregister
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.combined_loginregister.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity() {
+    lateinit var binding: ActivityUserBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
